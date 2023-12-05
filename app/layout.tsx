a8a64269d10315afae58038cd6706ebe38e7ab6e
Finessetro <prid.tann@gmail.com>
2023-12-04 22:16:47 -0500

    Load Pokemon data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-
 export default function RootLayout({
   children,
 }: {
@@ -6,7 +5,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ backgroundColor: 'gray' }}>{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,51 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+import {readFileSync} from 'fs'
+import {parse} from 'papaparse'
+
+const pokemon_file = readFileSync('data/PokeStats.csv')
+var pokemon_data = []
+var parse_err = null
+parse(pokemon_file.toString(), {
+  complete: function(results) {
+    pokemon_data = results.data
+    parse_err = results.error
+  }
+})
+while (pokemon_data.length == 0 && parse_err == null) {}
+
+function StatBox({stat_name, stat_value}) {
+  return(
+    <div>
+      <div>
+        {stat_name}
+      </div>
+      <div>
+        {stat_value}
+      </div>
+    </div>
+  )
 }
 
+function StatsBox({ stat_values }) {
+  const stat_names = ['HP', 'ATK', 'DEF', 'SP. ATK', 'SP. DEF', 'SPD', 'BST']
+  var stats_list = [];
+  for (var i = 0; i < stat_values.length; i++) {
+    stats_list.push(<StatBox stat_name={stat_names[i]} stat_value={stat_values[i]}/>)
+  }
+  console.log(stats_list)
+  return(
+    <div>
+      {stats_list}
+    </div>
+  )
+}
+
+export default function Page() {
+  const pokemon_idx = Math.floor(Math.random() * (pokemon_data.length-1))
+  const chosen_pokemon = pokemon_data[pokemon_idx]
+  console.log(chosen_pokemon)
+  var pokemon_stats = chosen_pokemon.slice(chosen_pokemon.length-7)
+  pokemon_stats = pokemon_stats.slice(1).concat(pokemon_stats[0])
+  return (
+    <StatsBox stat_values={pokemon_stats}/>
+  )
+}
